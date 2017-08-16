c2ae53282d82ba84805c9ee7dde549dd25c47008
CE Test Archivist <arch@hothstor2.phy.bnl.gov>
2017-08-15 21:42:24 -0400

    First support for femb.

---
+++ femb-summary.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+'''This script produces a single JSON file from all those in one sample directory.
+
+Seed it with the fembTest_powercycle_test/params.json file
+'''
+
+# /dsk/1/data/sync-json/hothdaq1/dsk/1/oper/femb/wib_sbnd_v109_femb_protodune_v308/20170803T171304/fembTest_powercycle_test/params.json
+
+import os
+import sys
+import json
+from glob import glob
+from collections import Counter
+
+def load_maybe_broken_json(filename):
+    '''Load result file, possibly as fscked up JSON.
+    '''
+    dat = open(filename).read()
+    for n in range(3):
+        dat = json.loads(dat)
+        if type(dat) == dict:
+            return dat
+    raise ValueError("Could not load result file %s" % resultfile)
+
+def fix_result(res):
+    # $@#*!
+    for maybe_garbage in ["adc_asics","fe_asics"]:
+        try:
+            val = res[maybe_garbage];
+        except KeyError:
+            return res
+
+        if ',' in val[0][0]:
+            val[0] = map(str, split(val[0][0],','))
+    return res
+
+def summarize_result(res):
+    try:
+        results = res["results"]
+    except KeyError:
+        return res
+    asic_fail = 0
+    chan_fail = 0
+    for one in results:
+        fail = one["fail"]=="1"
+        if not fail:
+            continue
+        if one.has_key("asic"):
+            asic_fail += 1
+        if one.has_key("ch"):
+            chan_fail += 1
+    results_summary = dict(asic_fail = asic_fail, chan_fail=chan_fail)
+    res["results_summary"] = results_summary
+    return res
+
+def slurp_from_seed(check_setup_params_json):
+    '''Surp a result starting with the check_setup/params.json file.
+    Return big dict keyed by data subdir name.
+
+    '''
+    seed = os.path.realpath(check_setup_params_json)
+    resdir = os.path.dirname(os.path.dirname(seed))
+
+    ret = dict()
+
+    for parfile in glob(os.path.join(resdir, "*/params.json")):
+        pardat = json.load(open(parfile))
+        dsd = pardat['datasubdir']
+        resdat = list()
+        for resfile in glob(parfile.replace("params.json","*-results.json")):
+            one = summarize_result(fix_result(load_maybe_broken_json(resfile)))
+            resdat.append(one)
+        #sys.stderr.write("%s: %s\n" % (dsd,len(resdat)))
+        ret[dsd] = dict(params=pardat, results=resdat)
+
+    return dict(femb=ret)
+
+if '__main__' == __name__:
+    dat = slurp_from_seed(sys.argv[1])
+    sys.stdout.write(json.dumps(dat, indent=4))
+    
--- taxon/femb.py
+++ taxon/femb.py
@@ -1,10 +1,13 @@
 from util import *
+import sys
 
 #
 # The FEMB taxon
 #
 def seeder(bld, **params):
-    return dataroot(bld).ant_glob("*/dsk/*/oper/femb/*/*/fembTest_powercycle_test/params.json")
+    ret = dataroot(bld).ant_glob("*/dsk/*/oper/femb/*/*/fembTest_powercycle_test/params.json")
+    print "#femb:\t%d" % len(ret)
+    return ret
 
 
 def builder(bld, seed_node, **params):
@@ -20,10 +23,28 @@ def builder(bld, seed_node, **params):
         
 
     jparam = json.loads(seed_node.read())
-    adcids = [str(jparam["adc_asics"][0][ind]) for ind in range(8)]
-    feids =  [str(jparam[ "fe_asics"][0][ind]) for ind in range(8)]
-    fmid = jparam["fm_ids"][0]
-    ts = jparam["session_start_time"]
+
+    # Some entries have garbage JSON
+    for maybe_garbage in ["adc_asics","fe_asics"]:
+        val = jparam[maybe_garbage];
+        if ',' in val[0][0]:
+            val[0] = map(str, split(val[0][0],','))
+
+    ts = str(jparam["session_start_time"])
+
+    # make up a "board" ID
+    bid = '-'.join([ str(jparam["box_ids"][0]),
+                     str(jparam["fm_ids"][0]),
+                     str(jparam["am_ids"][0])])
+    ident = '-'.join([bid, ts])
+
+    try:
+        adcids = [str(jparam["adc_asics"][0][ind]) for ind in range(8)]
+        feids =  [str(jparam[ "fe_asics"][0][ind]) for ind in range(8)]
+    except IndexError:
+        sys.stderr.write("Failed to get expected asics for %s:\nadc:%s\nfe:%s\n"% (seed_node.abspath(), jparam["adc_asics"], jparam["fe_asics"]))
+        return
+
 
     taxon = 'femb'
     jq_node = jq_file(bld, taxon)
@@ -33,7 +54,25 @@ def builder(bld, seed_node, **params):
 
     png_nodes = basedir.ant_glob("*/*.png")
     
+    injester = bld.path.find_resource("femb-summary.py")
+    bld(rule="${SRC[0]} ${SRC[1]} > ${TGT}",
+        source=[injester, seed_node], target=[json_node])
 
-    print seed_node
-    pass
+    subdir = install_path(taxon, "femb-"+bid, ts)
+    reltoroot = '/'.join(['..']*len(subdir.split('/')))
+
+    bld(rule="${YASHA} -I.. -o ${TGT[0]} -v reltoroot %s -V ${SRC[1]} ${SRC[0]}" % reltoroot,
+        source=[j2_node, json_node], target=[html_node])
+
+    out = os.path.join("${PREFIX}", subdir)
+    bld.install_as(os.path.join(out, "index.html"), html_node)
+
+    png_nodes = basedir.ant_glob("*/*.png")
+    if png_nodes:
+        bld.install_files(out, png_nodes)
+
+    return dict(ident=ident, serial=bid, adcids=adcids, feids=feids, timestamp=ts, board_id=bid, 
+                json_node = json_node, html_node=html_node, png_nodes = png_nodes,
+                html_subdir = subdir)
+
 
